944c786563049ccb631d72112961a62bc7254875
Dhruv Kunjumon <dhruvsci30@gmail.com>
2023-08-16 02:52:09 +0530

    Revert "restructure configs"
    This reverts commit 9cf465887160ead2907fa861e396a362832660d6.

--- nvim/.config/nvim/lua/custom/configs/options.lua
+++ nvim/.config/nvim/lua/custom/configs/options.lua
@@ -1,6 +1,3 @@
--- [[ Setting options ]]
--- See `:help vim.o`
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -13,41 +10,14 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 
-vim.opt.colorcolumn = "80"
-
--- Set highlight on search
-vim.o.hlsearch = false
-vim.opt.incsearch = true
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
--- vim.o.mouse = 'a'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
 vim.opt.updatetime = 50
-vim.o.timeoutlen = 300
 
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
-
+vim.opt.colorcolumn = "80"
 
--- nvim/.config/nvim/lua/custom/plugins/gopls.lua
+++ nvim/.config/nvim/lua/custom/plugins/gopls.lua
@@ -2,7 +2,7 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "gopls",
+        "gopls,"
       },
     },
 }
