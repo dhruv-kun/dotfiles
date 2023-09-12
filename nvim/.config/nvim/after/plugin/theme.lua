91a0fa338b8c5b24001443c2e1f5c8eeb040586d
Dhruv Kunjumon <dhruvsci30@gmail.com>
2023-09-12 15:24:26 +0530

    added line number highlights

---
+++ nvim/.config/nvim/after/plugin/theme.lua
@@ -0,0 +1,7 @@
+function LineNumberColors()
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=true })
+end
+
+LineNumberColors()
